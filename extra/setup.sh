93da46c83f9fbf28d3487a59e48a06ed842e9d93
Philippe Coval <philippe.coval@osg.samsung.com>
2018-01-29 10:56:29 +0100

    WIP: mod: extra/setup.sh (sandbox/rzr/tizen/1.2-rel)
    Change-Id: Ib3e4edcc08bc67bdc1707e6d46ce780940fdd0f2
    Signed-off-by: Philippe Coval <philippe.coval@osg.samsung.com>

--- extra/setup.sh
+++ extra/setup.sh
@@ -183,7 +183,7 @@ build_()
 #           url="http://github.com/iotivity/${package}"
 #           branch="master"
             url="http://github.com/tizenteam/${package}"
-            branch="sandbox/pcoval/on/master/tizen_common"
+            branch="sandbox/rzr/1.2-rel"
             args=" -b "${branch}""
             args=" $args --depth 1"
             ls "$package" || \
